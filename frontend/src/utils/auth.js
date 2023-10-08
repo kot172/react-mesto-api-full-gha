1fadf697e0729173c9be7d4c23fa43722ed7069a
Nikita Bogomazov <Bnikita72@yandex.ru>
2023-10-08 18:27:52 +0500

    fix auth

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,10 +1,19 @@
 export const BASE_URL = "http://mesto.nikita.back.nomoredomainsrocks.ru";
 
+const checkResponse = (res) => {
+  if (res.ok) {
+    return res.json();
+  } else {
+    return Promise.reject(`Ошибка ${res.status}`);
+  }
+};
+
+
 export const register = (email, password) => {
   return fetch(`${BASE_URL}/signup`, {
     method: "POST",
     headers: {
-      Accept: "application/json",
+      // Accept: "application/json",
       "Content-Type": "application/json",
     },
     body: JSON.stringify({ email, password }),
@@ -19,7 +28,7 @@ export const login = (email, password) => {
   return fetch(`${BASE_URL}/signin`, {
     method: "POST",
     headers: {
-      Accept: "application/json",
+      // Accept: "application/json",
       "Content-Type": "application/json",
     },
     body: JSON.stringify({ email, password }),
@@ -45,12 +54,4 @@ export const getContent = (token) => {
   })
     .then((res) => checkResponse(res))
     .then((data) => data);
-};
-
-const checkResponse = (res) => {
-  if (res.ok) {
-    return res.json();
-  } else {
-    return Promise.reject(`Ошибка ${res.status}`);
-  }
 };
