990aee7d9f6998b2cdcf37921547f88702de3492
Nikita Bogomazov <Bnikita72@yandex.ru>
2023-10-10 22:37:30 +0500

    Fixed errors

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ const cors = require('cors');
 const rateLimit = require('express-rate-limit');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const errorHandler = require('./middlewares/error-handler');
-const { MongoClient, ServerApiVersion } = require('mongodb')
+// const { MongoClient, ServerApiVersion } = require('mongodb')
 
 
 const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/testdb' } = process.env;
